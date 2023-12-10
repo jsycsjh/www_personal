82a5d705715d90dd35a90236694a1a6553d364ae
Jianhui Sun <js9gu@virginia.edu>
2023-12-09 21:51:14 -0500

    test

--- __deploy.sh
+++ __deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Upload to server
-scp -r _site/* jsycsjh@jsycsjh.github.io:/
+scp -r _site/* jsycsjh@jsycsjh.github.io:public_html/
 
 # make sure to fix the permissions on the server
 # chmod -R 750 *
